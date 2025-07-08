7167a6dde29d2c65e8cd7409b47490d58a5441c8
subhamgupta212002 <guptasubham797@gmail.com>
2025-07-08 22:29:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -204,8 +204,8 @@ consumption, rainfall impact, and groundwater depletion rates.""")
             - Incorporated bulk file uploads for large-scale text predictions.
             - Provided dynamic visual feedback and personalized profile sections.
              """)
-    st.markdown("- [🔗 **💡 LENS_R eXpert -**](https://github.com/subhamgupta212002/recommended/blob/main/test.py)")
-    st.markdown("- [ **Streamlit -**](https://recommend2025.streamlit.app/)")
+            st.markdown("- [🔗 **💡 LENS_R eXpert -**](https://github.com/subhamgupta212002/recommended/blob/main/test.py)")
+            st.markdown("- [ **Streamlit -**](https://recommend2025.streamlit.app/)")
     
     st.header("🎓 Education")
     st.write("""**- Bachelor of Business Administrative (BBA)**- Llyod Group of Institute Management and Technology, Greater Noida, Graduated
